9a1e5a731c4600af7a84fcee654989d65e6f0c22
harshabhat88 <harshabhat88@gmail.com>
2019-06-28 11:18:55 +0000

    Check the firefox version on a remore servers
    Pull the list of Firefox version from the list of servers

---
+++ Firefox version.ps1
@@ -0,0 +1,20 @@
+﻿$array =@() 
+$keyname = 'SOFTWARE\\mozilla.org\\Mozilla' 
+$computernames = Get-Content C:\Computer.txt 
+foreach ($server in $computernames) 
+{ 
+$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $server) 
+$key = $reg.OpenSubkey($keyname) 
+$value = $key.GetValue('CurrentVersion') 
+ $obj = New-Object PSObject 
+         
+        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $server 
+         
+        $obj | Add-Member -MemberType NoteProperty -Name "FireFoxVersion" -Value $value 
+ 
+        $array += $obj  
+ 
+ 
+} 
+ 
+$array | select ComputerName,FirefoxVersion | export-csv C:\Firefox_Version.csv
